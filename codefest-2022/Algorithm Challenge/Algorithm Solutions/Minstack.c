523fef53af9acbb98e0037b73f6411494cb69049
root <root@DESKTOP-67JJD6O>
2024-10-26 05:23:10 +0000

    added files

---
+++ codefest-2022/Algorithm Challenge/Algorithm Solutions/Minstack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INIT_SIZE 5
+/**
+*  */
+typedef struct _stack
+{
+    int capacity;
+    int *stack;
+    int *top;
+}stack;
+
+int main (void)
+{
+    return (0);
+}

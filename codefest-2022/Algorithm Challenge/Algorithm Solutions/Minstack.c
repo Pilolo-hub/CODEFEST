ce7e0364b828b2bf06e480a6b0d72f3ae6fedf79
root <root@DESKTOP-67JJD6O>
2024-10-26 05:49:11 +0000

    added files

--- codefest-2022/Algorithm Challenge/Algorithm Solutions/Minstack.c
+++ codefest-2022/Algorithm Challenge/Algorithm Solutions/Minstack.c
@@ -3,13 +3,45 @@
 
 #define INIT_SIZE 5
 /**
-*  */
-typedef struct _stack
+* struct _minstack- stack of integers
+* @capacity: capacity of stack
+* @size: size of stack
+* @arr: pointer to stack array
+* @top: top of stack
+* @minarr: stack of the minimum value at every point
+*/
+typedef struct _minStack
 {
     int capacity;
-    int *stack;
+    int size;
+    int *arr;
     int *top;
-}stack;
+    int *minarr;
+
+}minStack;
+
+minStack *MyStack(void)
+{
+    minStack *s = (minStack *)malloc(sizeof(minStack));
+    s->arr = (int *)malloc(sizeof(int) * INIT_SIZE);
+    s->minarr = (int *)malloc(sizeof(int) * INIT_SIZE);
+    s->capacity = INIT_SIZE;
+    s->top = NULL;
+    s->size = 0;
+    return (s);
+}
+
+void push(minStack *s, int val)
+{
+    s->arr[s->size] = val;
+    if (s->size && val >= s->minarr[s->size - 1])
+        s->minarr[s->size] = s->minarr[s->size - 1];
+    else
+        s->minarr[s->size] = val;
+    s->top = &s->arr[s->size];
+    s->size++;
+}
+
 
 int main (void)
 {
